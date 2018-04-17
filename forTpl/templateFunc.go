18b860012b7862493827741c1768ba2c640762f1
Jerry <shijianling@nncz.cn>
2018-04-17 20:16:02 +0800

    getDateStr

--- forTpl/templateFunc.go
+++ forTpl/templateFunc.go
@@ -2,8 +2,10 @@ package forTpl
 
 import (
 	"strconv"
+	"time"
 )
 
+//转换价格  分==>元
 func GetPrice(price interface{}) string {
 	value1, ok := price.(int64)
 	var re float64
@@ -19,4 +21,13 @@ func GetPrice(price interface{}) string {
 	priceFormat := strconv.FormatFloat(re, 'f', -1, 32)
 
 	return priceFormat
+}
+
+//根据时间戳获取日期字符创
+func GetDateHM(timestamp int64) string {
+	if timestamp <= 0 {
+		return ""
+	}
+	tm := time.Unix(timestamp, 0)
+	return tm.Format("2006-01-02 15:04")
 }
--- forTpl/templateFunc_test.go
+++ forTpl/templateFunc_test.go
@@ -4,12 +4,15 @@ import (
 	"testing"
 
 	"github.com/astaxie/beego"
-
-	_ "github.com/astaxie/beego"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func Test_GetPrice(t *testing.T) {
-	cc := GetPrice(100)
+func Test_GetDateHM(t *testing.T) {
+	cc := GetDateHM(1523967273)
 	beego.Debug("Hello World!", cc)
 }
+
+// func Test_GetPrice(t *testing.T) {
+// 	cc := GetPrice(100)
+// 	beego.Debug("Hello World!", cc)
+// }
