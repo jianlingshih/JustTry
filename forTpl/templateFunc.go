c36b913f037c6de5c53030ac00b74dd266176358
Jerry <shijianling@nncz.cn>
2018-04-27 13:32:00 +0800

    date minus

--- forTpl/templateFunc.go
+++ forTpl/templateFunc.go
@@ -25,6 +25,18 @@ func GetPrice(price interface{}) string {
 
 	return priceFormat
 }
+func GetDateAdd() {
+	//Add方法和Sub方法是相反的，获取t0和t1的时间距离d是使用Sub，将t0加d获取t1就是使用Add方法
+	k := time.Now()
+	//一天之前
+	d, _ := time.ParseDuration("-24h")
+	fmt.Println(k.Add(d))
+	//一周之前
+	fmt.Println(time.Unix(k.Add(d*7).Unix(), 0).Format("20060102"))
+	//一月之前
+	fmt.Println(k.Add(d * 30))
+
+}
 
 //根据时间戳获取日期字符创
 func GetDateHM(timestamp int64) string {
