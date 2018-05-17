47d24d336464576a6291e8907f64df7fbcb7f880
Jerry <shijianling@nncz.cn>
2018-05-17 17:42:33 +0800

    mysql 正则查询

--- sql/common.sql
+++ sql/common.sql
@@ -4,3 +4,6 @@ INSERT INTO cms_customers (openid) SELECT "iwnxaninasx" FROM dual
 
  # 简易计算
  SELECT ROUND(1.2333,3)
+
+# 正则 查询 
+ select  * FROM cms_customers_child where childid REGEXP ( SELECT CONCAT('^', REPLACE (child_ids, ',', '$|^'), '$')   FROM  cms_customers_order   WHERE orderid='1805171637792028' )
