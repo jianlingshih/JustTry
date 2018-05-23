31163076a9f740d3822c5c50898b06f9ef65ef95
Jerry <shijianling@nncz.cn>
2018-05-23 15:17:09 +0800

    查询

--- sql/common.sql
+++ sql/common.sql
@@ -7,3 +7,9 @@ INSERT INTO cms_customers (openid) SELECT "iwnxaninasx" FROM dual
 
 # 正则 查询 
  select  * FROM cms_customers_child where childid REGEXP ( SELECT CONCAT('^', REPLACE (child_ids, ',', '$|^'), '$')   FROM  cms_customers_order   WHERE orderid='1805171637792028' )
+
+#日期查询  
+SELECT  FROM_UNIXTIME(1526217150, '%Y-%m-%d %H:%i:%S')
+
+# 带IF查询
+SELECT *   FROM  `table` WHERE IF(  `parentID` =1,  `plan_id` <10,  `plan_id` >500 ) LIMIT 0 , 30 
