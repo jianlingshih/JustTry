3db4132eddec40d8f32fd3452652c6b40dfd3433
Jerry <shijianling@nncz.cn>
2018-06-26 15:55:24 +0800

    读取txt文档 wordbyword

--- forTpl/templateFunc.go
+++ forTpl/templateFunc.go
@@ -22,6 +22,33 @@ import (
 )
 
 //读取txt文档
+func wordByWord(file string) error {
+	var err error
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		line, err := r.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Printf("error reading file %s", err)
+			return err
+		}
+		r := regexp.MustCompile("[^\\s]+")
+		words := r.FindAllString(line, -1)
+		for i := 0; i < len(words); i++ {
+			fmt.Println(words[i])
+		}
+	}
+	return nil
+}
+
+//读取txt文档
 func lineByLine(file string) error {
 	var err error
 
