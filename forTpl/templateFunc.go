6c68b9988c3512e75482e46844a311edf911dbb3
Jerry <shijianling@nncz.cn>
2018-06-28 12:34:41 +0800

    读取txt 按字符

--- forTpl/templateFunc.go
+++ forTpl/templateFunc.go
@@ -22,6 +22,32 @@ import (
 )
 
 //读取txt文档
+func charByChar(file string) error {
+	var err error
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		line, err := r.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Printf("error reading file %s", err)
+			return err
+		}
+
+		for _, x := range line {
+			fmt.Println(string(x))
+		}
+	}
+	return nil
+}
+
+//读取txt文档
 func wordByWord(file string) error {
 	var err error
 	f, err := os.Open(file)
